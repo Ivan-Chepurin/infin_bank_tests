0d2c5f7a31afc67eebe0c239f773d9b8853c82e2
Ivan-Chepurin <76522796+Ivan-Chepurin@users.noreply.github.com>
2022-07-06 13:36:51 +0500

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -4,10 +4,9 @@ from utils.webdriver import DriverManager
 
 @fixture
 def selenium_browser_chrome(context):
-    # -- HINT: @behave.fixture is similar to @contextlib.contextmanager
     context.driver = None
     yield context.driver
-    # -- CLEANUP-FIXTURE PART:
+    
     context.driver.close()
 
 
